6014424b15b8fba09d0bc71927422f5493c978d1
tahanae <ctaxyhw@gmail.com>
2021-12-19 01:40:26 +0900

    ユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :fairst_name, :last_name, :fairst_name_furigana, :last_name_furigana, :dete_of_birth])
+  end
 end
