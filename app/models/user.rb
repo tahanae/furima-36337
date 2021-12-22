e15a2699e46e95a990832601a5551d5e7640017d
tahanae <ctaxyhw@gmail.com>
2021-12-23 04:57:33 +0900

    テストコード修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
+  #has_many :items
 
+    # バリデーションの設定(空の文字列を保存させない為と一意性制約)
     validates :name,                presence: true
-    validates :password,            presence: true, length: { minimum: 6}
-    validates :email,               presence: true, format: {with: /\A\S+@\S+\.\S+\z/ }
+    validates :email,               presence: true, format: { with: /\A\S+@\S+\.\S+\z/ }
+    validates :encrypted_password,  presence: true, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/ }
     validates :first_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :last_name,           presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name_furigana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
     validates :last_name_furigana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
     validates :date_of_birth,       presence: true, format: {with: /\A\d{4}-\d{2}-\d{2}\z/ }
     
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    first_name            {'test'}
-    last_name             {'test'}
-    first_name_furigana   {'test'}
-    last_name_furigana    {'test'}
+    first_name            {'山田'}
+    last_name             {'太郎'}
+    first_name_furigana   {'ヤマダ'}
+    last_name_furigana    {'タロウ'}
     date_of_birth         {'2000-01-01'}
   end
 end
