ff2bb15fac0bd05c214dfe3146d8fd055efa3ac4
tahanae <ctaxyhw@gmail.com>
2022-01-03 01:36:28 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,30 +2,20 @@
     belongs_to :user
     has_one_attached :image
 
-
-    # ジャンルの選択が「--」の時は保存できない
-    with_options presence: true do
-      validates :title
-      validates :explanation
-      validates :category_id
-      validates :condition_id
-      validates :DeriveryCharge_id
-      validates :area_id
-      validates :ScheduledDelivery_id
-      validates :user
-    end
+      validates :image,                presence: true
+      validates :title,                presence: true, length: { maximum: 40 }
+      validates :explanation,          presence: true, length: { maximum: 1000 }
+      validates :category_id,          presence: true, numericality: { other_than: 1 }
+      validates :condition_id,         presence: true, numericality: { other_than: 1 }
+      validates :DeriveryCharge_id,    presence: true, numericality: { other_than: 1 }
+      validates :area_id,              presence: true, numericality: { other_than: 1 }
+      validates :ScheduledDelivery_id, presence: true, numericality: { other_than: 1 }
+      validates :user,                 presence: true
 
 
-    validates :category_id,          numericality: { other_than: 1 }
-    validates :condition_id,         numericality: { other_than: 1 }
-    validates :DeriveryCharge_id,    numericality: { other_than: 1 }
-    validates :area_id,              numericality: { other_than: 1 }
-    validates :ScheduledDelivery_id, numericality: { other_than: 1 }
-
-    
     # 価格は、¥300 ~ ¥9,999,999の間のみ保存可能
     with_options presence: true, format: {with: /\A[0-9]+\z/ } do
-      validates :price, numericality: { only_integer: true, greater_then_or_equal_to:300, less_then_or_eqal_to:9_999_999 },
+      validates :price, numericality: { only_integer: true, greater_than_or_equal_to:300, less_than_or_equal_to:9_999_999 },
                         presence: { message: "can't be blank" }
     end
 
