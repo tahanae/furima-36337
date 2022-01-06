c04492d09e7161133b75bd7544203c07a3aeec3c
tahanae <ctaxyhw@gmail.com>
2022-01-06 20:01:20 +0900

    商品出品機能修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new, :create]
 #  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,14 +10,9 @@
       validates :derivery_charge_id,    presence: true, numericality: { other_than: 1 }
       validates :area_id,              presence: true, numericality: { other_than: 1 }
       validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1 }
-      validates :user,                 presence: true
-
 
     # 価格は、¥300 ~ ¥9,999,999の間のみ保存可能
-    with_options presence: true, format: {with: /\A[0-9]+\z/ } do
-      validates :price, numericality: { only_integer: true, greater_than_or_equal_to:300, less_than_or_equal_to:9999999 },
-                        presence: { message: "can't be blank" }
-    end
+      validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to:300, less_than_or_equal_to:9999999 }
 
     def was_attached?
       self.image.attached?
@@ -29,6 +24,5 @@
     belongs_to_active_hash :derivery_charge
     belongs_to_active_hash :area
     belongs_to_active_hash :scheduled_delivery
-
-  end
   
+  end
